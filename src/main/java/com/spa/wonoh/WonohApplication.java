33b60751169d371c47bae7bd2073c51e7bca9a83
wonoh <wldnjsdh44121@nate.com>
2019-01-10 13:41:30 +0900

    init

---
+++ src/main/java/com/spa/wonoh/WonohApplication.java
@@ -0,0 +1,14 @@
+package com.spa.wonoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonohApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WonohApplication.class, args);
+    }
+
+}
+
